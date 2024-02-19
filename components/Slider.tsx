edfb1c714cf909ce4a0dbb8ef1bda0de6f23ffae
lsgonc <lucasscgoncalves@gmail.com>
2024-02-19 20:48:53 -0300

    configurando responsividade em telas menores

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -2,10 +2,9 @@ export default function Slider()
 {
     return(
         <>
-        <div className="flex justify-center items-center bg-comp_default h-80 px-20">
-            <h1 className="font-bold text-3xl text-center">Seja bem-vindo ao EduClicker Explore salas de aula interativas, descubra quizzes personalizados e mergulhe no aprendizado de uma forma única. Educação reinventada.</h1>
+        <div className="flex justify-center items-center bg-comp_default h-80 px-20 max-sm:text-left text-center">
+            <h1 className="font-bold text-3xl max-sm:text-lg ">Seja bem-vindo ao EduClicker Explore salas de aula interativas, descubra quizzes personalizados e mergulhe no aprendizado de uma forma única. Educação reinventada.</h1>
         </div>
         </>
-        
     );
 }
--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -8,8 +8,8 @@ type props = {
 export default function Wrapper(props: { text: string; button_text: string; }) {
     return (
         <>
-        <div className="flex mx-40 bg-comp_default rounded-md my-20 py-8 flex justify-between items-center px-20">
-            <h2>{props.text}</h2>
+        <div className="flex md:mx-40 bg-comp_default rounded-md my-20 py-8 flex justify-between items-center px-10 sm:px-20">
+            <h2 className="font-bold">{props.text}</h2>
            <button>
             <div className="bg-bg_default w-44 py-4 rounded-md text-white">
             <p>{props.button_text}</p>
