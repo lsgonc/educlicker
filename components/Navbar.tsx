8cf11248a4b453e0cffcc20d1c36599f58993f0e
lsgonc <lucasscgoncalves@gmail.com>
2024-02-19 19:24:33 -0300

    componente da navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,13 +3,13 @@ export default function Navbar()
     return (
         <>
         <nav>
-            <div className="flex bg-comp_default">
-                <h1>LOGO</h1>
-                <ul>
-                    <li><a href=""></a>Home</li>
-                    <li><a href=""></a>Login</li>
-                    <li><a href=""></a>Sobre</li>
-                    <li><a href=""></a>Contato</li>
+            <div className="flex bg-comp_default py-6 px-20 max-sm:px-5">
+                <h1 className="font-bold"><a href="">LOGO</a></h1>
+                <ul className="flex ml-auto gap-5">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Login</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Contato</a></li>
                 </ul>
             </div>
         </nav>
