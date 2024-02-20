6d8b2b510e10aebcda9950133889eda60ebe742d
LuizOt31 <luiz.mello@estudante.ufscar.br>
2024-02-20 16:56:14 -0300

    pagina de login, correção de link navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar()
                 <h1 className="font-bold"><a href="">LOGO</a></h1>
                 <ul className="flex ml-auto gap-5">
                     <li><a className="hover:text-bg_default" href="">Home</a></li>
-                    <li><a className="hover:text-bg_default" href="">Login</a></li>
+                    <li><a className="hover:text-bg_default" href="/login">Login</a></li>
                     <li><a className="hover:text-bg_default" href="">Sobre</a></li>
                     <li><a className="hover:text-bg_default" href="">Contato</a></li>
                 </ul>
