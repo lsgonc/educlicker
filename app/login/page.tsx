b7b3009bd94aa7851ed84307d818c8d0d1ff3d99
LuizOt31 <luiz.mello@estudante.ufscar.br>
2024-02-21 22:33:39 -0300

    tela de cadastro

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,21 @@
 import Button from "@/components/Button";
+import Input from "@/components/Input"
 
 export default function Login() {
     return (
         <main className="w-full min-w-screen h-full min-h-screen bg-[#D9E0D4] flex justify-center items-center">
             <div className="bg-[#2F364D] w-[28rem] h-[32rem] rounded-3xl flex flex-col justify-center items-center ">
 
-                <label htmlFor="usuario" className="flex flex-col justify-center items-start text-white">
+                <label htmlFor="usuario" className="flex flex-col text-white">
                     Nome de usuário
-                    <Button nome="usuario" text="" tipo_input="text" color="bg-[#D9E0D4]" valor=""/>
+                    <Input type="text"/>
                 </label>
                 <label htmlFor="senha" className="flex flex-col justify-center items-start text-white">
                     Senha
-                    <Button nome="senha" text="" tipo_input="text" color="bg-[#D9E0D4]" valor=""/>
+                    <Input type="text"/>
                 </label>
+                <Button text="ENTRAR" />
+                <a href="/cadastro" className="text-white underline">Não possuí cadastro?</a>
 
             </div>
         </main>
